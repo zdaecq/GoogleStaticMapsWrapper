82ba09bc54125e10e93252f9afcbca280bd75949
Yaroslav Bondar <bonyadmitr@gmail.com>
2016-09-02 18:10:53 +0300

    Initial Commit

---
+++ GoogleStaticMapsWrapper/GoogleStaticMapsWrapper.h
@@ -0,0 +1,19 @@
+//
+//  GoogleStaticMapsWrapper.h
+//  GoogleStaticMapsWrapper
+//
+//  Created by zdaecqze zdaecq on 02.09.16.
+//  Copyright © 2016 Bondar Yaroslav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoogleStaticMapsWrapper.
+FOUNDATION_EXPORT double GoogleStaticMapsWrapperVersionNumber;
+
+//! Project version string for GoogleStaticMapsWrapper.
+FOUNDATION_EXPORT const unsigned char GoogleStaticMapsWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleStaticMapsWrapper/PublicHeader.h>
+
+
